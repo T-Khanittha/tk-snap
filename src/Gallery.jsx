57535b5c7d23a1e400488f8753e15170de3778f2
T-Khanittha <tarn.khanit@odds.team>
2023-10-20 00:27:28 +0700

    Add simple Home and Gallery pages

---
+++ src/Gallery.jsx
@@ -0,0 +1,13 @@
+// src/components/Gallery.js
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <div>
+      <h1>Gallery Page</h1>
+      {/* Add content for the gallery page */}
+    </div>
+  );
+};
+
+export default Gallery;
---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+// src/components/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      {/* Add content for the homepage */}
+    </div>
+  );
+};
+
+export default Home;
